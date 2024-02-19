8948c34f914b733850001bf3c76bf379e5c1cbb3
Ferengi2002 <patronjuan20@gmail.com>
2024-02-18 23:59:54 -0500

    commit2

---
+++ src/main/java/G3_Inmuebles/vistasGrpo3/GestionInmuebles/InterfazInmueble/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package G3_Inmuebles.vistasGrpo3.GestionInmuebles.InterfazInmueble;
+
+/**
+ *
+ * @author MyHP
+ */
+public class NewClass {
+    
+        public static void main(String[] args) {
+        Modulossss abrirModulos = new Modulossss();
+        abrirModulos.setVisible(true);
+    }
+    
+}
