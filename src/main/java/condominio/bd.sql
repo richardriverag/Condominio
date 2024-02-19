11e6458ef01bba821566f2bb865c615fab791aec
Richard Rivera <rich7mcs@hotmail.com>
2024-02-19 17:32:54 -0500

    bd script database and user table

--- src/main/java/condominio/bd.sql
+++ src/main/java/condominio/bd.sql
@@ -6,4 +6,16 @@
  * Author:  Richard
  * Created: 16 feb 2024
  */
+create database condominio;
+use condominio;
 
+CREATE TABLE Usuario (
+  id_usuario int,
+  usuario varchar(50) NOT NULL,
+  contrasenia varchar(50) NOT NULL,
+  nombre varchar(10) NOT NULL,
+  apellido varchar(100) NOT NULL,
+  email varchar(10) NOT NULL,
+  tipoUsuario int DEFAULT NULL,
+  PRIMARY KEY (id_usuario)
+);
