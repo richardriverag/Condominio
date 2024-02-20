0ec23a520f17bfb58bf21d569d1ce4aaf0be38b0
Usuario <Usuario@LAPTOP-CAV0ND72>
2024-02-20 12:13:04 -0500

    G5 funcionalidades de los botones tablas con la base de datos

--- src/main/java/G5_CheckIn/ParqueaderoVisita.java
+++ src/main/java/G5_CheckIn/ParqueaderoVisita.java
@@ -4,18 +4,43 @@
  */
 package G5_CheckIn;
 
+import condominio.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Usuario
  */
 public class ParqueaderoVisita extends javax.swing.JFrame {
- String parqueadero = "";
-    /**
-     * Creates new form ParqueaderoVisita
-     */
+ //conexion
+    Conexion con = new Conexion();
+    Connection cn = con.getCon();
+    
+    //varaibles globales
+    int parqueadero = 0;
+     
+    //tablas 
+    DefaultTableModel modelo;
+    String estado;
+    int idTabla=2;
+    String[] Titulos={"Numero Parqueadero","Estado"};
+    String nombreTabla = "Select * from  ParqueaderoVisita";
+    //  ORDER BY [Fecha de Ingreso] DESC, [Hora de Ingreso] DESC
+
     public ParqueaderoVisita() {
         initComponents();
-       
+        modelo = new DefaultTableModel();
+        modelo.addColumn("Numero Parqueadero");
+        modelo.addColumn("Estado"); 
+        JTableParqueadero.setModel(modelo);
+        
+        cargarTabla(modelo, JTableParqueadero, nombreTabla, Titulos, idTabla);
     }
 
     /**
@@ -28,13 +53,14 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        JTableParqueadero = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jLabel14 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        JTableParqueadero.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null},
                 {null, null},
@@ -51,7 +77,12 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
                 "Numero Parqueadero", "Estado"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        JTableParqueadero.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JTableParqueaderoMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(JTableParqueadero);
 
         jButton1.setText("Aceptar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -63,6 +94,8 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
         jLabel14.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
         jLabel14.setText("Parqueadero Visitas");
 
+        jLabel19.setText("Selecciona el parqueadero:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -70,13 +103,15 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(33, 33, 33)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(41, 41, 41)
-                        .addComponent(jLabel14)))
+                        .addComponent(jLabel14))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel19)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(21, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -84,7 +119,9 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel14)
-                .addGap(24, 24, 24)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel19)
+                .addGap(4, 4, 4)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -95,12 +132,25 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        parqueadero = "1";
+        estado = "Ocupado";
+        String sql = "Update ParqueaderoVisita SET estado = '"+estado+"' WHERE idParqueadero ="+parqueadero;
+        ejecutar(sql);
         
-        CheckIn ck = new CheckIn(parqueadero);
+        CheckIn ck = new CheckIn(String.valueOf(parqueadero));
+        ck.setVisible(true);
         this.setVisible(false);
         
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void JTableParqueaderoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JTableParqueaderoMouseClicked
+        int fila = JTableParqueadero.getSelectedRow();  
+        if (fila == -1) 
+        {
+            JOptionPane.showMessageDialog(null, "No se ha seleccionado la fila");
+        }else{
+            parqueadero = Integer.parseInt((String) JTableParqueadero.getValueAt(fila,0).toString());
+        }
+    }//GEN-LAST:event_JTableParqueaderoMouseClicked
 
     /**
      * @param args the command line arguments
@@ -138,10 +188,47 @@ public class ParqueaderoVisita extends javax.swing.JFrame {
         });
     }
 
+        public void ejecutar (String mysql){
+        try 
+        { 
+            //Conectamos a la base de datos y le pasamos la sentencia mysql 
+            PreparedStatement pps = cn.prepareStatement(mysql);
+            //se ejecuta todo lo anterior en mysql.
+            pps.executeUpdate(); 
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("error");  
+        }  
+    }
+      
+    public void cargarTabla(DefaultTableModel tblAux, JTable tblTablaCine,String nombreTabla,String[] Titulos,int id){
+       tblAux.setRowCount(0);
+        try {
+        PreparedStatement pps = cn.prepareStatement(nombreTabla);    
+        ResultSet rs = pps.executeQuery();
+            tblAux = (DefaultTableModel) tblTablaCine.getModel();
+            Object[] variableTabla = new Object[id];
+            while (rs.next()){
+                try{
+                    for(int i=0;i<id;i++){
+                       variableTabla[i]=rs.getObject(Titulos[i]);
+                    }
+                }catch(SQLException ex){
+                        System.out.println("error Cargar Tabla");  
+                }
+                tblAux.addRow(variableTabla);
+            }
+            tblTablaCine.setModel(tblAux);
+        } catch (Exception ex) {
+            System.out.println("error2");      
+        }
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable JTableParqueadero;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
