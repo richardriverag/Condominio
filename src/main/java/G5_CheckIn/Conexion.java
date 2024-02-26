cb8ef5741d1875d75496bc65ece710e3967cae55
Usuario <Usuario@LAPTOP-CAV0ND72>
2024-02-26 16:26:52 -0500

    G5 agregar tablas necesarias para G5

---
+++ src/main/java/G5_CheckIn/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package G5_CheckIn;
+
+/**
+ *
+ * @author Usuario
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Conexion {
+
+        Connection con;
+
+        // Funcion que va conectarse a mi base de datos de mysql 
+        public Connection conectar(){
+        // Reseteamos a null la conexion a la bd
+        con=null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Nos conectamos a la base de datos
+            
+            con= (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/condominio", "root", "Hidalgo.2212");
+            if(con !=null){
+               // JOptionPane.showMessageDialog(null, "Conexion existosa", "alert", JOptionPane.ERROR_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(null, "Conexion NO existosa", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        // Si la conexion NO fue exitosa mostramos un mensaje de error
+        catch (ClassNotFoundException | SQLException e){
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, e.getMessage(), "alert", JOptionPane.ERROR_MESSAGE);
+        }
+        return con;
+        }
+        
+
+        
+    }
+    
